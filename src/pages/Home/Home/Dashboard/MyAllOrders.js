07c96fcf0163d7eb21dd8a036b7a278531e98a93
mahim-shariar <mdmahim924214@gmail.co>
2022-01-03 13:12:40 +0600

    plish

--- src/pages/Home/Home/Dashboard/MyAllOrders.js
+++ src/pages/Home/Home/Dashboard/MyAllOrders.js
@@ -1,12 +1,12 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 
 
 const MyAllOrders = (props) => {
-  const { name, discountPrice, date, image,_id } = props.order;
-  const [control,setControl]=useState(false)
+  const { name, discountPrice, date, image, _id } = props.order;
+  const [control, setControl] = useState(false)
 
   const handleDelete = (id) => {
     fetch(`https://stormy-wave-87937.herokuapp.com/myAllOrder/${id}`, {
@@ -22,37 +22,37 @@ const MyAllOrders = (props) => {
           )
         }
         // window.location.reload()       
-      })   
+      })
   }
 
-    return (
-        <Grid item xs={12} md={3}>
-            <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="220"
-        image={image}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Order Date: {date}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Price: {discountPrice}
-        </Typography>
-         </CardContent>
+  return (
+    <Grid item xs={12} md={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="220"
+          image={image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Order Date: {date}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Price: {discountPrice}
+          </Typography>
+        </CardContent>
         <CardActions>
-        <Button onClick={()=>handleDelete(_id)} variant="outlined" startIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-         </CardActions>
-        </Card>
-        </Grid>
-    );
+          <Button onClick={() => handleDelete(_id)} variant="outlined" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 };
 
 export default MyAllOrders;
