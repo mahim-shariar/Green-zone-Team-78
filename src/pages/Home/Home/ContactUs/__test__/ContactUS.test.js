4a9010828be89ea291f9237adcc23d53ef09105c
Musfiqur Rahma <musfiq371@gmail.com>
2022-01-05 23:29:30 +0600

    'added-meetbenefit'

--- src/pages/Home/Home/ContactUs/__test__/ContactUS.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { ContactUs } from '../ContactUS';
-
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDom.render(<ContactUs></ContactUs>, div);
-})
